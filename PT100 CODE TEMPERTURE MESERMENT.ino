e87ac930a010d5e13b11c699e6bae2d7d0d7e4eb
Aniket Gangawane <aniketgangavane2003@gmail.com>
2025-02-14 20:43:13 +0530

    Update PT100 CODE TEMPERTURE MESERMENT.ino

--- PT100 CODE TEMPERTURE MESERMENT.ino
+++ PT100 CODE TEMPERTURE MESERMENT.ino
@@ -4,31 +4,22 @@
 \\DATA PIN AND VCC IN BETWEEN CONNECT 1K OHM RESISTAN 
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
 #define ONE_WIRE_BUS 8 //conection  of data pin to D8
-
 OneWire oneWire(ONE_WIRE_BUS);
-
 DallasTemperature sensors(&oneWire);
-
 float Celsius = 0;
 float Fahrenheit = 0;
-
 void setup() {
   sensors.begin();
   Serial.begin(9600);
 }
-
 void loop() {
   sensors.requestTemperatures();
-
   Celsius = sensors.getTempCByIndex(0);
   Fahrenheit = sensors.toFahrenheit(Celsius);
-
   Serial.print(Celsius);
   Serial.print(" C  ");
   Serial.print(Fahrenheit);
   Serial.println(" F");
-
   delay(1000);
 }
